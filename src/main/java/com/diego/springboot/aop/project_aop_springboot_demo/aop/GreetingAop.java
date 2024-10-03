ccc9134af2fb1768eaf0eefd172c50d0f1b7a5b8
Diego Fernando Guerrero Perez <diego.guerrero9612@gmail.com>
2024-10-03 17:14:23 -0500

    Se agrego un nuevo metodo around en el Greeting aop

--- src/main/java/com/diego/springboot/aop/project_aop_springboot_demo/aop/GreetingAop.java
+++ src/main/java/com/diego/springboot/aop/project_aop_springboot_demo/aop/GreetingAop.java
@@ -3,6 +3,7 @@ package com.diego.springboot.aop.project_aop_springboot_demo.aop;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -43,6 +44,25 @@ public class GreetingAop {
         String method = joinPoint.getSignature().getName();
         String args = Arrays.toString(joinPoint.getArgs());
         logger.info("despues lanzar una excepcion:" + method + "con los argumentos "+ args);
+
+    }
+
+    @Around("execution(String com.diego.springboot.aop.project_aop_springboot_demo.services.GreetingService.*(..))")
+    public Object loggerAround(ProceedingJoinPoint joinPoint) throws Throwable{
+
+        String method = joinPoint.getSignature().getName();
+        String  args = Arrays.toString(joinPoint.getArgs());
+
+        Object result = null;
+        try {
+            logger.info("el metodo" + method + "() con los parametros" + args);
+            result = joinPoint.proceed();
+            logger.info("El metodo " + method + "Retorna el resultado " +  result);
+            return result;
+        } catch (Throwable e){
+            logger.error("Error en la llamada del metodo " + method + "()");
+            throw e;
+        }
 
     }
 
